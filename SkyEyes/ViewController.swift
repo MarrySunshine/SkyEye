ebe0b1ace1a455932b09cb9a3648af5b86574b37
MarrySunshine <wei1996zero@hotmail.com>
2016-11-17 20:47:01 +0800

    Initial Commit

---
+++ SkyEyes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyEyes
+//
+//  Created by 鲁厶维 on 2016/11/17.
+//  Copyright © 2016年 鲁厶维. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
